0e4fa609ef131348b9b163b9749e617c91efbd1b
Chaoxu Tong <chaoxu@uber.com>
2018-04-15 14:40:02 -0700

    refactor

---
+++ src/Graph.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class Graph {
+  int eidx, n, m;
+  // edge arrays
+  int[] ehead, eprev;
+  // node arrays
+  int[] elast;
+
+  int einv(int e) {
+    return e^1;
+  }
+
+  int etail(int e) {
+    return ehead[einv(e)];
+  }
+
+  Graph(int nNodes, int nEdges) {
+    n = nNodes;
+    m = nEdges;
+    eidx = 0;
+
+    ehead = new int[2*m]; eprev = new int[2*m];
+    elast = new int[n];
+    Arrays.fill(elast, -1);
+  }
+
+  int addEdge(int u, int v) {
+    ehead[eidx] = v; eprev[eidx] = elast[u]; elast[u] = eidx++;
+    ehead[eidx] = u; eprev[eidx] = elast[v]; elast[v] = eidx++;
+    return elast[u];
+  }
+}
--- src/MaxFlow.java
+++ src/MaxFlow.java
@@ -1,42 +1,33 @@
-import java.util.*;
+import java.util.Arrays;
 
 class MaxFlow {
-  int eidx, n, m;
+  Graph g;
   long f;
   // edge arrays
   long[] flow, capa;
-  int[] ehead, eprev;
   // node arrays
-  int[] now, level, elast;
+  int[] now, level;
 
   long resCapa(int e) {
     return capa[e] - flow[e];
   }
 
-  int etail(int e) {
-    return ehead[e^1];
-  }
-
-  MaxFlow(int nNodes, int nEdges) {
-    n = nNodes;
-    m = nEdges;
-    eidx = 0;
-
-    flow = new long[2 * m]; capa = new long[2 * m];
-    ehead = new int[2 * m]; eprev = new int[2 * m]; elast = new int[n];
-    level = new int[n]; now = new int[n];
-    Arrays.fill(elast, -1);
+  MaxFlow(Graph g) {
+    this.g = g;
+    flow = new long[2 * g.m]; capa = new long[2 * g.m];
+    level = new int[g.n]; now = new int[g.n];
   }
 
   int addEdge(int u, int v, long capacity) {
-    ehead[eidx] = v; flow[eidx] = 0; capa[eidx] = capacity; eprev[eidx] = elast[u]; elast[u] = eidx++;
-    ehead[eidx] = u; flow[eidx] = 0; capa[eidx] = 0; eprev[eidx] = elast[v]; elast[v] = eidx++;
-    return elast[u];
+    int e = g.addEdge(u, v);
+    flow[e]  = 0; capa[e] = capacity;
+    flow[g.einv(e)] = 0; capa[g.einv(e)] = 0;
+    return e;
   }
 
   long dinic(int source, int sink) {
     while (bfs(source, sink)) {
-      System.arraycopy(elast, 0, now, 0, n);
+      System.arraycopy(g.elast, 0, now, 0, g.n);
       while (dfs(source, Long.MAX_VALUE, sink) > 0);
     }
     return f;
@@ -45,15 +36,15 @@ class MaxFlow {
   boolean bfs(int source, int sink) {
     Arrays.fill(level, -1);
     int front = 0, back = 0;
-    int[] queue = new int[n];
+    int[] queue = new int[g.n];
 
     level[source] = 0;
     queue[back++] = source;
 
     while (front < back && level[sink] == -1) {
       int u = queue[front++];
-      for (int e = elast[u]; e != -1; e = eprev[e]) {
-        int v = ehead[e];
+      for (int e = g.elast[u]; e != -1; e = g.eprev[e]) {
+        int v = g.ehead[e];
         if (level[v] != -1 || resCapa(e) == 0) continue;
         level[v] = level[u] + 1;
         queue[back++] = v;
@@ -69,13 +60,13 @@ class MaxFlow {
       return curDelta;
     }
 
-    for (int e = now[u]; e != -1; now[u] = e = eprev[e]) {
-      int v = ehead[e];
+    for (int e = now[u]; e != -1; now[u] = e = g.eprev[e]) {
+      int v = g.ehead[e];
       if (level[v] <= level[u] || resCapa(e) == 0) continue;
       long delta = dfs(v, Math.min(curDelta, resCapa(e)), sink);
       if (delta == 0) continue;
       flow[e] += delta;
-      flow[e ^ 1] -= delta;
+      flow[g.einv(e)] -= delta;
       return delta;
     }
     return 0;
