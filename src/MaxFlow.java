4cda1cefcd54118436c8a94496dc0c0cbbbcc75d
Chaoxu Tong <chaoxu@uber.com>
2018-04-15 14:23:23 -0700

    update

--- src/MaxFlow.java
+++ src/MaxFlow.java
@@ -1,77 +1,83 @@
 import java.util.*;
 
 class MaxFlow {
-    long[] flow, capa;
-    int[] now, level, eadj, eprev, elast;
-    int eidx, N, M;
-    long totalFlow;
+  int eidx, n, m;
+  long f;
+  // edge arrays
+  long[] flow, capa;
+  int[] ehead, eprev;
+  // node arrays
+  int[] now, level, elast;
 
-    MaxFlow(int nodes, int edges) {
-        this.N = nodes;
-        this.M = edges;
+  long resCapa(int e) {
+    return capa[e] - flow[e];
+  }
 
-        flow = new long[2*M];
-        capa = new long[2*M];
-        eadj = new int[2*M];
-        eprev = new int[2*M];
-        elast = new int[N];
-        level = new int[N];
-        now = new int[N];
-        Arrays.fill(elast, -1);
-        eidx = 0;
+  int etail(int e) {
+    return ehead[e^1];
+  }
+
+  MaxFlow(int nNodes, int nEdges) {
+    n = nNodes;
+    m = nEdges;
+    eidx = 0;
+
+    flow = new long[2 * m]; capa = new long[2 * m];
+    ehead = new int[2 * m]; eprev = new int[2 * m]; elast = new int[n];
+    level = new int[n]; now = new int[n];
+    Arrays.fill(elast, -1);
+  }
+
+  int addEdge(int u, int v, long capacity) {
+    ehead[eidx] = v; flow[eidx] = 0; capa[eidx] = capacity; eprev[eidx] = elast[u]; elast[u] = eidx++;
+    ehead[eidx] = u; flow[eidx] = 0; capa[eidx] = 0; eprev[eidx] = elast[v]; elast[v] = eidx++;
+    return elast[u];
+  }
+
+  long dinic(int source, int sink) {
+    while (bfs(source, sink)) {
+      System.arraycopy(elast, 0, now, 0, n);
+      while (dfs(source, Long.MAX_VALUE, sink) > 0);
+    }
+    return f;
+  }
+
+  boolean bfs(int source, int sink) {
+    Arrays.fill(level, -1);
+    int front = 0, back = 0;
+    int[] queue = new int[n];
+
+    level[source] = 0;
+    queue[back++] = source;
+
+    while (front < back && level[sink] == -1) {
+      int u = queue[front++];
+      for (int e = elast[u]; e != -1; e = eprev[e]) {
+        int v = ehead[e];
+        if (level[v] != -1 || resCapa(e) == 0) continue;
+        level[v] = level[u] + 1;
+        queue[back++] = v;
+      }
     }
 
-    void add_edge(int a, int b, long c) {
-        eadj[eidx] = b; flow[eidx] = 0; capa[eidx] = c; eprev[eidx] = elast[a]; elast[a] = eidx++;
-        eadj[eidx] = a; flow[eidx] = 0; capa[eidx] = 0; eprev[eidx] = elast[b]; elast[b] = eidx++;
+    return level[sink] != -1;
+  }
+
+  long dfs(int u, long curDelta, int sink) {
+    if (u == sink) {
+      f += curDelta;
+      return curDelta;
     }
 
-    long dinic(int source, int sink) {
-        while (bfs(source, sink)) {
-            System.arraycopy(elast, 0, now, 0, N);
-            while (dfs(source, Long.MAX_VALUE, sink) > 0);
-        }
-        return totalFlow;
+    for (int e = now[u]; e != -1; now[u] = e = eprev[e]) {
+      int v = ehead[e];
+      if (level[v] <= level[u] || resCapa(e) == 0) continue;
+      long delta = dfs(v, Math.min(curDelta, resCapa(e)), sink);
+      if (delta == 0) continue;
+      flow[e] += delta;
+      flow[e ^ 1] -= delta;
+      return delta;
     }
-
-    boolean bfs(int source, int sink) {
-        Arrays.fill(level, -1);
-        int front = 0, back = 0;
-        int[] queue = new int[N];
-
-        level[source] = 0;
-        queue[back++] = source;
-
-        while (front < back && level[sink] == -1) {
-            int node = queue[front++];
-            for (int e = elast[node]; e != -1; e = eprev[e]) {
-                int to = eadj[e];
-                if (level[to] == -1 && flow[e] < capa[e]) {
-                    level[to] = level[node] + 1;
-                    queue[back++] = to;
-                }
-            }
-        }
-
-        return level[sink] != -1;
-    }
-
-    long dfs(int cur, long curflow, int goal) {
-        if (cur == goal) {
-            totalFlow += curflow;
-            return curflow;
-        }
-
-        for (int e = now[cur]; e != -1; now[cur] = e = eprev[e]) {
-            if (level[eadj[e]] > level[cur] && flow[e] < capa[e]) {
-                long res = dfs(eadj[e], Math.min(curflow, capa[e] - flow[e]), goal);
-                if (res > 0) {
-                    flow[e] += res;
-                    flow[e ^ 1] -= res;
-                    return res;
-                }
-            }
-        }
-        return 0;
-    }
+    return 0;
+  }
 }
